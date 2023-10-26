eef39b8fa51f5ab3c60580ab4ae92b25c5aaa0fd
Jashim <jashim.vanover@gmail.com>
2023-10-26 12:59:57 -0400

    changes

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,3 +1,21 @@
-function showAlert(number) {
-    alert(`Button ${number} was clicked.`);
+let display = document.getElementById('display');
+let input = '';
+
+function appendToDisplay(value) {
+    input += value;
+    display.value = input;
+}
+
+function clearDisplay() {
+    input = '';
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        input = eval(input).toString();
+        display.value = input;
+    } catch (error) {
+        display.value = 'Error';
+    }
 }
